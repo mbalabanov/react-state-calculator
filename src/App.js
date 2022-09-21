49d6d8a3189047ae615d2894a3fd09ae236e3b13
Marin Balabanov <marincomics@gmail.com>
2022-09-21 16:51:13 +0200

    Adds first optimizations

--- src/App.js
+++ src/App.js
@@ -12,28 +12,17 @@ function App() {
 
   const [state, setState] = useState(initialState)
 
-  const leftNumberClick = (event) => {
-
+  const numberClick = (event) => {
     const newState = {
-      leftPane: state.leftPane + event.target.value,
+      leftPane: state.leftPane,
       selectedOperator: state.selectedOperator,
       rightPane: state.rightPane,
       calculatedResult: state.calculatedResult
     }
-    if (newState.leftPane !== '0') {
-      setState(newState)
-    }
-  }
 
-  const rightNumberClick = (event) => {
-    const newState = {
-      leftPane: state.leftPane,
-      selectedOperator: state.selectedOperator,
-      rightPane: state.rightPane + event.target.value,
-      calculatedResult: state.calculatedResult
-    }
-    
-    if (newState.rightPane !== '0') {
+    newState[event.target.className] += event.target.value
+
+    if (newState[event.target.className] !== '0') {
       setState(newState)
     }
   }
@@ -41,32 +30,23 @@ function App() {
   const operatorClick = (event) => {
     const newState = {
       leftPane: state.leftPane,
-      selectedOperator: event.target.value,
-      rightPane: state.rightPane,
-      calculatedResult: state.calculatedResult
-    }
-    setState(newState)
-  }
-
-  const clearLeftValue = (event) => {
-    console.log(event.target.className)
-    const newState = {
-      leftPane: '',
       selectedOperator: state.selectedOperator,
       rightPane: state.rightPane,
       calculatedResult: state.calculatedResult
     }
+    newState.selectedOperator = event.target.value
+
     setState(newState)
   }
 
-  const clearRightValue = (event) => {
-    console.log(event.target.className)
+  const clearValue = (event) => {
     const newState = {
       leftPane: state.leftPane,
       selectedOperator: state.selectedOperator,
-      rightPane: '',
+      rightPane: state.rightPane,
       calculatedResult: state.calculatedResult
     }
+    newState[event.target.className] = ''
     setState(newState)
   }
 
@@ -93,6 +73,7 @@ function App() {
     if (state.selectedOperator === '/') {
       newState.calculatedResult = state.leftPane / state.rightPane
     }
+
     setState(newState)
   }
 
@@ -101,17 +82,17 @@ function App() {
       <div className="panel">
         <p className="numberField">{state.leftPane ? state.leftPane : 0}</p>
         <div className="numbers">
-          <button value="1" className="leftPane" onClick={leftNumberClick}>1</button>
-          <button value="2" className="leftPane" onClick={leftNumberClick}>2</button>
-          <button value="3" className="leftPane" onClick={leftNumberClick}>3</button>
-          <button value="4" className="leftPane" onClick={leftNumberClick}>4</button>
-          <button value="5" className="leftPane" onClick={leftNumberClick}>5</button>
-          <button value="6" className="leftPane" onClick={leftNumberClick}>6</button>
-          <button value="7" className="leftPane" onClick={leftNumberClick}>7</button>
-          <button value="8" className="leftPane" onClick={leftNumberClick}>8</button>
-          <button value="9" className="leftPane" onClick={leftNumberClick}>9</button>
-          <button value="0" className="leftPane" onClick={leftNumberClick}>0</button>
-          <button className="leftPane" onClick={clearLeftValue}>Clear</button>
+          <button value="1" className="leftPane" onClick={numberClick}>1</button>
+          <button value="2" className="leftPane" onClick={numberClick}>2</button>
+          <button value="3" className="leftPane" onClick={numberClick}>3</button>
+          <button value="4" className="leftPane" onClick={numberClick}>4</button>
+          <button value="5" className="leftPane" onClick={numberClick}>5</button>
+          <button value="6" className="leftPane" onClick={numberClick}>6</button>
+          <button value="7" className="leftPane" onClick={numberClick}>7</button>
+          <button value="8" className="leftPane" onClick={numberClick}>8</button>
+          <button value="9" className="leftPane" onClick={numberClick}>9</button>
+          <button value="0" className="leftPane" onClick={numberClick}>0</button>
+          <button className="leftPane" onClick={clearValue}>Clear</button>
         </div>
       </div>
 
@@ -128,17 +109,17 @@ function App() {
       <div className="panel">
         <p className="numberField">{state.rightPane ? state.rightPane : 0}</p>
         <div className="numbers">
-          <button value="1" className="rightPane" onClick={rightNumberClick}>1</button>
-          <button value="2" className="rightPane" onClick={rightNumberClick}>2</button>
-          <button value="3" className="rightPane" onClick={rightNumberClick}>3</button>
-          <button value="4" className="rightPane" onClick={rightNumberClick}>4</button>
-          <button value="5" className="rightPane" onClick={rightNumberClick}>5</button>
-          <button value="6" className="rightPane" onClick={rightNumberClick}>6</button>
-          <button value="7" className="rightPane" onClick={rightNumberClick}>7</button>
-          <button value="8" className="rightPane" onClick={rightNumberClick}>8</button>
-          <button value="9" className="rightPane" onClick={rightNumberClick}>9</button>
-          <button value="0" className="rightPane" onClick={rightNumberClick}>0</button>
-          <button className="rightPane" onClick={clearRightValue}>Clear</button>
+          <button value="1" className="rightPane" onClick={numberClick}>1</button>
+          <button value="2" className="rightPane" onClick={numberClick}>2</button>
+          <button value="3" className="rightPane" onClick={numberClick}>3</button>
+          <button value="4" className="rightPane" onClick={numberClick}>4</button>
+          <button value="5" className="rightPane" onClick={numberClick}>5</button>
+          <button value="6" className="rightPane" onClick={numberClick}>6</button>
+          <button value="7" className="rightPane" onClick={numberClick}>7</button>
+          <button value="8" className="rightPane" onClick={numberClick}>8</button>
+          <button value="9" className="rightPane" onClick={numberClick}>9</button>
+          <button value="0" className="rightPane" onClick={numberClick}>0</button>
+          <button className="rightPane" onClick={clearValue}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
